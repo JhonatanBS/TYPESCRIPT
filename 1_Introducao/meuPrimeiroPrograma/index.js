93342b59678dc1aed2b28a6880107ace1ff289e1
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-06 10:49:21 -0300

    Meu primeiro programa com TypeScrypt

---
+++ 1_Introducao/meuPrimeiroPrograma/index.js
@@ -0,0 +1,8 @@
+// let firstName = "Lucas"
+// let lastName = "Silva"
+// let x = true
+// let num = 50
+function nameFull(nome, sobrenome) {
+    console.log("Me chamo " + nome + " " + sobrenome);
+}
+nameFull("Lucas", "Silva");
---
+++ 1_Introducao/meuPrimeiroPrograma/index.ts
@@ -0,0 +1,10 @@
+// let firstName = "Lucas"
+// let lastName = "Silva"
+// let x = true
+// let num = 50
+
+function nome(nome: string, sobrenome: string){
+    console.log("Me chamo " + nome + " "+ sobrenome);
+}
+      
+nameFull("Lucas","Silva");      
