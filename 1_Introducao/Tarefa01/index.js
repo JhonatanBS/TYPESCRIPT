5b365d0ac383d4de8435873d12eaa00dccf8c763
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-06 11:08:15 -0300

    Tarefa 01

---
+++ 1_Introducao/Tarefa01/index.js
@@ -0,0 +1,4 @@
+function soma(numberOne, numberTwo) {
+    console.log(numberOne + numberTwo);
+}
+sum(15, 20);
---
+++ 1_Introducao/Tarefa01/index.ts
@@ -0,0 +1,5 @@
+function sum(numberOne: number,numberTwo: number){
+    console.log(numberOne + numberTwo);
+}
+
+sum(15,20);
