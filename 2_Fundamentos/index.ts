30302cd7b23a215420783fad237a1baaaeb8c650
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-06 12:00:17 -0300

    Annotation e Inference

--- 2_Fundamentos/index.ts
+++ 2_Fundamentos/index.ts
@@ -14,3 +14,12 @@ console.log(nome, typeof nome);
 // 3 - boolean
 let lampada: boolean= true;
 console.log(lampada, typeof lampada);
+
+/* INFERENCE E ANNOTATION */
+
+// annotation: O programador declara o tipo de dado
+const ann: string= "annotation";
+
+// inference: O Typescrypt detecta o tipo de dado e atribuí
+let inf = "inference";
+let num = 50;
