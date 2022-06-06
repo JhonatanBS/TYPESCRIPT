ba3a85f9c41ba7990845b0ccf5b498d689ec8861
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-06 11:44:43 -0300

    Tipos primitivos de dados em Typescrypt: number,string e boolean

---
+++ 2_Fundamentos/index.js
@@ -0,0 +1,12 @@
+/* TIPOS DE DADOS */
+// 1 - number
+var a = 100;
+var b = 200;
+console.log(a + b);
+console.log(a, typeof a, b, typeof b);
+// 2 - string
+var nome = "Paulo";
+console.log(nome, typeof nome);
+// 3 - boolean
+var lampada = true;
+console.log(lampada, typeof lampada);
---
+++ 2_Fundamentos/index.ts
@@ -0,0 +1,16 @@
+/* TIPOS DE DADOS */
+
+// 1 - number
+let a: number = 100;
+let b: number = 200;
+
+console.log(a + b);
+console.log(a, typeof a, b, typeof b);
+
+// 2 - string
+let nome: string = "Paulo";
+console.log(nome, typeof nome);
+
+// 3 - boolean
+let lampada: boolean= true;
+console.log(lampada, typeof lampada);
