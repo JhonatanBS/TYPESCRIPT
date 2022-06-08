201188c5165639cc99904124bd6171a78db81154
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-08 07:56:44 -0300

    Sintaxe ou tipagem de arrays: number[] e string[]

---
+++ 3_Avancando/index.js
@@ -0,0 +1,3 @@
+/* Tipagem de Arrays */
+var numeros = [1, 3, 5, 7, 9, 11, 15];
+numeros.push("string");
---
+++ 3_Avancando/index.ts
@@ -0,0 +1,11 @@
+/* Tipagem de Arrays */
+
+const numeros: number[] = [1,3,5,7,9,11,15]; 
+
+// Não é possível inserir um dado do tipo string em um array numérico
+//numeros.push("string");
+
+const names: string[] = [ "João", "Paulo", "Matheus"];
+
+// Não é possível inserir um dado do tipo numérico em um array de string
+//names.push(5);
