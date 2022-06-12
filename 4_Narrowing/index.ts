68d1fa61e910e42f94833bc2ee4c5241b61e84a6
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-12 10:10:40 -0300

    Operador in

--- 4_Narrowing/index.ts
+++ 4_Narrowing/index.ts
@@ -67,4 +67,29 @@ class User {
   userGreeting(jhon);
   userGreeting(paul);
 
+  /* Operador in */
+  class Dog {
+    name
+    breed
   
+    constructor(name: string, breed?: string) {
+      this.name = name
+      if (breed) {
+        this.breed = breed
+      }
+    }
+  }
+  
+  const turca = new Dog('Turca')
+  const bob = new Dog('Bob', 'Pastor Alemão')
+  
+  function showDogDetails(dog: Dog) {
+    if ('breed' in dog) {
+      console.log(`O cachorro é da raça: ${dog.breed}`)
+    } else {
+      console.log('O cachorro é um SRD!')
+    }
+  }
+  
+  showDogDetails(turca)
+  showDogDetails(bob)
