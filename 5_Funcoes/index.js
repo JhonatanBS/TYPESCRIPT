d71da5855281fe425637ac59f74dd4859e6ae21d
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-14 08:48:10 -0300

    Função callback como argumento

--- 5_Funcoes/index.js
+++ 5_Funcoes/index.js
@@ -1,4 +1,18 @@
 /* Function void: Não retorna nada */
 function welcome() {
     console.log("Seja bem vindo!");
+    //return 0; Erro ao tentar retorna algo
 }
+welcome();
+/* Function callback como argumento */
+var firstName = function (name) { return "Meu nome \u00E9 ".concat(name); };
+var fullName = function (f, firstName, lastName) {
+    console.log("Dados:");
+    if (lastName) {
+        console.log("".concat(f(firstName), " ").concat(lastName));
+        return;
+    }
+    console.log("".concat(f(firstName)));
+};
+fullName(firstName, "Paulo", "Silva");
+fullName(firstName, "Matheus");
--- 5_Funcoes/index.ts
+++ 5_Funcoes/index.ts
@@ -4,3 +4,23 @@ function welcome():void{
     console.log("Seja bem vindo!");
     //return 0; Erro ao tentar retorna algo
 }
+
+welcome();
+
+/* Function callback como argumento */
+
+const firstName = (name: string):string => `Meu nome é ${name}`;
+
+const fullName = (f: (name:string) => string, firstName:string, lastName?: string) => {
+   console.log("Dados:");
+   
+   if(lastName){
+    console.log(`${f(firstName)} ${lastName}`);
+    return;
+   }
+
+   console.log(`${f(firstName)}`);
+}
+
+fullName(firstName, "Paulo", "Silva");
+fullName(firstName, "Matheus");
