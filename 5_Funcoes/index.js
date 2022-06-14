12b797ba38747b33a3d78d5ac21ba97d725b97df
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-14 07:59:02 -0300

    Função void é uma função que não retorna nenhum dado

---
+++ 5_Funcoes/index.js
@@ -0,0 +1,4 @@
+/* Function void: Não retorna nada */
+function welcome() {
+    console.log("Seja bem vindo!");
+}
---
+++ 5_Funcoes/index.ts
@@ -0,0 +1,6 @@
+/* Function void: Não retorna nada */
+
+function welcome():void{
+    console.log("Seja bem vindo!");
+    //return 0; Erro ao tentar retorna algo
+}
