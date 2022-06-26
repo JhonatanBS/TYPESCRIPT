87ef450c432357d1c076a4e445df40e46b72a0e4
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-26 08:50:32 -0300

    Restringindo funções generics com constrains

--- 7_Criacao_de_tipos/index.js
+++ 7_Criacao_de_tipos/index.js
@@ -4,3 +4,8 @@ var code = function (codigo) {
 };
 console.log(code("1231564fdsf"));
 console.log(code(15545455));
+/* Constrains em Generics */
+function sum(a, b) {
+    return "A soma dos n\u00FAmeros ".concat(a, " e ").concat(b, " \u00E9 ").concat(a + b);
+}
+console.log(sum(120, 50));
--- 7_Criacao_de_tipos/index.ts
+++ 7_Criacao_de_tipos/index.ts
@@ -5,3 +5,11 @@ const code = <T>(codigo: T):string => {
 }
 console.log(code("1231564fdsf"));
 console.log(code(15545455));
+
+/* Constrains em Generics */
+
+function sum<T extends number>(a: T , b: T):string{
+    return `A soma dos números ${a} e ${b} é ${ a + b}`;
+}
+
+console.log(sum(120,50));
