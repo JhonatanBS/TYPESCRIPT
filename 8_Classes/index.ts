0f894698604972b13e1673297c012a8021abf0a5
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-08-02 13:09:32 -0300

    Herança com extends e super()

--- 8_Classes/index.ts
+++ 8_Classes/index.ts
@@ -40,3 +40,36 @@ const Uno = new Car("Uno");
 // Uno.brand = "Audi"; Erro
 
 console.log(Uno);
+
+/* 
+   Herança: Quando um classe filho recebe as propriedades de outra classe pai, usamos o 'extends'.
+   super(): É chamado quando herdamos propriedades de outra classe.
+*/
+
+class Pessoa {
+  name: string;
+  age: number;
+  sex: string;
+
+  constructor(name: string, age: number, sex: string) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+  }
+}
+
+class Employee extends Pessoa{
+    salary: number;
+    hours: number;
+
+    constructor(name: string, age: number, sex: string,salary: number, hours: number) {
+      super(name,age,sex);
+      this.salary = salary;
+      this.hours = hours;
+    }
+}
+
+const Joana = new Pessoa("Joana", 25, "F");
+const engineer = new Employee("Marta",30, "F", 4500.50, 8);
+
+console.log(Joana, engineer);
