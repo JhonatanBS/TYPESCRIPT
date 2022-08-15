73773269c9eb46ac941e83b8a3b42cec3f2d6a4b
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-08-15 11:19:05 -0300

    export e import com desestruturação

---
+++ 9_Modules/dataPersonal.ts
@@ -0,0 +1,6 @@
+export const dataPersonal = {
+  name: "Lucas",
+  age: 25,  
+}
+
+// export { dataPersonal }; opção 2
--- 9_Modules/index.ts
+++ 9_Modules/index.ts
@@ -1,4 +1,11 @@
-// import default
+// export default e import
+
 import hello_world from "./hello_world";
 
 console.log(hello_world());
+
+// export and import destructuring
+
+import { dataPersonal } from "./dataPersonal";
+
+console.log(dataPersonal);
