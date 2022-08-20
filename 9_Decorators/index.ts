4101c2f400acfe8690808501447e15bcdc6b7cb0
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-08-20 12:29:42 -0300

    Múltiplos decorators

--- 9_Decorators/index.ts
+++ 9_Decorators/index.ts
@@ -11,3 +11,30 @@ class ExampleClass {
   @first()
   method() {}
 }
+
+// Multiplos Decorators
+
+function a() {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor){
+    console.log("Executando o decorator a")
+  }
+}
+
+function b() {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor){
+    console.log("Executando o decorator b")
+  }
+}
+
+function c() {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor){
+    console.log("Executando o decorator c")
+  }
+}
+
+class Example {
+  @c()
+  @b()
+  @a()
+  ordem() {};
+}
