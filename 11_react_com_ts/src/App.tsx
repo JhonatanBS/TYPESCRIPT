f07c5a39bfdd861904b7fa8b95c387b5093ed62a
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-08-23 09:34:22 -0300

    Funções no React.ts

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -5,12 +5,20 @@ export function App() {
   const isWorking: boolean = true;
   const salary: number = 5500;
 
+  /* Funções */
+
+  function employee(name: string, salary: number): string {
+    return `Meu nome é ${name} e recebo ${salary}.`;
+  }
+
   return (
     <div>
       <h1>React com Typescript</h1>
       <p>Nome: {name}</p>
       <p>Salário: {salary}</p>
       {isWorking && <p>Estou no trabalho</p>}
+
+      <div>{employee(name, salary)}</div>
     </div>
   )
 }
