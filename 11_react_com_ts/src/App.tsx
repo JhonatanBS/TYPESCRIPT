c7eda5e863b03e83c149ad352dddb4b2e7722396
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-08-23 09:03:04 -0300

    Variáveis no React.ts

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -1,8 +1,16 @@
 export function App() {
- 
+  /* Variáveis */
+  
+  const name: string = "Jhonatan";
+  const isWorking: boolean = true;
+  const salary: number = 5500;
+
   return (
     <div>
       <h1>React com Typescript</h1>
+      <p>Nome: {name}</p>
+      <p>Salário: {salary}</p>
+      {isWorking && <p>Estou no trabalho</p>}
     </div>
   )
 }
