89bb9135554ff06a4e71a1655e3f154f7a8ebd0c
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-08-31 14:57:57 -0300

    Created route get

--- 12_express_com_ts/src/index.ts
+++ 12_express_com_ts/src/index.ts
@@ -1,3 +1,16 @@
 /* Typescript + Express*/
 
 console.log("Started Typescript in Express");
+
+/* Express */
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.send("Ola Mundo");
+});
+
+app.listen(3333);
+ 
