e3e8410bb8970105676b04ae957e7ed5cd8c8501
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-09-04 22:01:22 -0300

    Import express and config

--- 13_api_restfull_com_ts/src/app.ts
+++ 13_api_restfull_com_ts/src/app.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import express from "express";
+import config from "config";
+
+const app = express();
+
+/* JSON Middleware */
+app.use(express.json());
+
+/* app port*/
+const port = config.get<number>("port");
+
+app.listen(port, async () => {
+  console.log("Aplicação rodando na porta 3000");  
+});
