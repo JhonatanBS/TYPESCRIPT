f8455192970ad8ddc4a2f2398587630be8e0e2e5
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-09-06 08:02:51 -0300

    Create variables sensitive

--- 13_api_restfull_com_ts/config/default.ts
+++ 13_api_restfull_com_ts/config/default.ts
@@ -1,4 +1,7 @@
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
 export default {
   port: 3000,
-  dbUri: "mongodb+srv://jhonatan:Xi2pdlpUc4nNvvgd@cluster0.voimkaj.mongodb.net/apirestfull?retryWrites=true&w=majority"  
+  dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.voimkaj.mongodb.net/apirestfull?retryWrites=true&w=majority` 
 };
